7e9bb7ce5f87e08a615ae37ab94d28c8ce416ff8
James Jiang <monochrome473@gmail.com>
2020-04-12 02:00:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
         four_pieces_owned: 200
       },
       :name => {
-        :reading_railroad => {},
+        reading_railroad: {},
         pennsylvania_railroad: {},
         b_and_o_railroad: {},
         shortline_railroad: {}
